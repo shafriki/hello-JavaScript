ef9201e3af2fc8e89f7d03cb9344fc4cacb603fa
shafriki <shafriki.cse@gmail.com>
2024-08-30 03:39:22 +0600

    complete tasks module 17

---
+++ arithmetic.js
@@ -0,0 +1,54 @@
+var orangePrice = 100;
+var applePrice = 130;
+var total = orangePrice + applePrice;
+
+// console.log(total);
+
+/*Task-1
+You went to the supermarket to buy some oranges and apples. Calculate how much money the shopkeeper will return.
+
+Input:
+The first line of the input is the taka you have. The second line is the cost of 1 kg of oranges and 1 kg of apples.
+
+Output:
+Print the result. */
+
+var totalTaka =1000;
+var orange =100;
+var apple =100;
+var cost = orange+apple;
+var moneyBack =totalTaka-cost;
+
+// console.log(moneyBack);
+
+/*Task-2
+Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+Input:
+The first line of the input is the marks of the five subjects mentioned above, respectively.
+
+Output:
+Print the result in 2 decimal places.*/
+
+var math =70; 
+var biology =80;
+var physics =74;
+var chemistry =80;
+var bangla =90;
+var totalMarks = math+biology+physics+chemistry+bangla;
+// console.log(totalMarks/5);
+
+/*Task-3
+You task is to divide the given number by 5 and show the remainder as the output.
+
+Input:
+The first line of the input contains the number.
+
+Output:
+Print the remainder.*/
+
+var num =119;
+var rem =119%5;
+console.log(rem);
+
+
--- practice.js
+++ practice.js
@@ -3,6 +3,10 @@ var amount = 100;
 var year = 1999;
 var date =23;
 
-console.log(amount);
-console.log(year);
-console.log(date);
+// console.log(amount);
+// console.log(year);
+// console.log(date);
+
+var name = "shafriki islam shemul";
+
+console.log(name);
